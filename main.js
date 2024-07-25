44820ab9793430f8fc8ab1710c055d886e79b905
Cameron Gaidys <93235025+Cameron-Gaidys-Github@users.noreply.github.com>
2024-07-25 17:03:27 -0400

    Numerous changes

--- main.js
+++ main.js
@@ -45,3 +45,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     showSlide(currentSlide);
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('header nav');
+
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+});
